f13bdc9e1533f3524a21bf58c3de0420ca48bbd8
Nikos Alexandris <nikos-alexandris@protonmail.com>
2022-02-17 21:18:48 +0200

    Initial commit

---
+++ string.c
@@ -0,0 +1,5 @@
+//
+// Created by nikos-alexandris on 2/17/22.
+//
+
+#include "string.h"
---
+++ string.h
@@ -0,0 +1,8 @@
+//
+// Created by nikos-alexandris on 2/17/22.
+//
+
+#ifndef STRING_STRING_H
+#define STRING_STRING_H
+
+#endif	// STRING_STRING_H
